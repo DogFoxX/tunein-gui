8048902d4b4045b15b04ad3d2306cfce965215b0
DogFoxX <creynierc@gmail.com>
2025-10-07 23:00:00 +0800

    Update build.rs and publish workflow

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,7 +1,3 @@
 fn main() {
-   // Only link to stdc++ on non-Windows targets
-    if !cfg!(target_env = "msvc") && !cfg!(target_env = "gnu") && !cfg!(target_os = "windows") {
-        println!("cargo:rustc-link-lib=dylib=stdc++");
-    }
-    tauri_build::build()
+   tauri_build::build()
 }
