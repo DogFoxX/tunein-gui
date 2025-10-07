22ebc9b33a3e0bd4e92fa942057bc0d9be83d378
DogFoxX <creynierc@gmail.com>
2025-10-07 22:48:21 +0800

    Update icons

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,4 +1,7 @@
 fn main() {
-    println!("cargo:rustc-link-lib=dylib=stdc++");
+   // Only link to stdc++ on non-Windows targets
+    if !cfg!(target_env = "msvc") && !cfg!(target_env = "gnu") && !cfg!(target_os = "windows") {
+        println!("cargo:rustc-link-lib=dylib=stdc++");
+    }
     tauri_build::build()
 }
